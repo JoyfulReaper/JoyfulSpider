e2810c7e60a4ab4b73b8e1a781c09606dbbd77a2
Kyle Givler <xvegan88x@gmail.com>
2020-11-25 17:21:00 -0500

    Add comments, and bool for following the rules

--- JoyfulSpider/JoyfulSpiderLibrary/GlobalConfig.cs
+++ JoyfulSpider/JoyfulSpiderLibrary/GlobalConfig.cs
@@ -34,19 +34,36 @@ namespace JoyfulSpider.Library
 {
     public static class GlobalConfig
     {
+        /// <summary>
+        /// Database connection
+        /// </summary>
         public static IDataConnection Connection { get; private set; }
-
+        /// <summary>
+        /// Robot's User-Agent
+        /// </summary>
         public static string UserAgent { get; set; } = "JoyfulSpider Alpha";
+        /// <summary>
+        /// Follow robots.txt rules
+        /// </summary>
+        public static bool FollowRobotRuels { get; set; } = true;
 
+        /// <summary>
+        /// Needed for log4net
+        /// </summary>
         private static ILoggerRepository logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
 
 
         static GlobalConfig()
         {
+            // Setup log4net
             var configFile = new FileInfo("log4net.config");
             XmlConfigurator.Configure(logRepository, configFile); 
         }
 
+        /// <summary>
+        /// Connect to the database
+        /// </summary>
+        /// <param name="type">Database type</param>
         public static void InitiliazeDataAccess(DataAccessType type)
         {
             if(type == DataAccessType.MSSQL)
@@ -56,7 +73,17 @@ namespace JoyfulSpider.Library
             }
         }
 
+        /// <summary>
+        /// Get a log object
+        /// </summary>
+        /// <param name="name">Name of the logger</param>
+        /// <returns>Log object</returns>
         public static ILog GetLogger(string name) => LogManager.GetLogger(name);
+
+        /// <summary>
+        /// Get a log obeject
+        /// </summary>
+        /// <returns>Log Object with default name</returns>
         public static ILog GetLogger() => LogManager.GetLogger("JoyfulSpider");
     }
 }
